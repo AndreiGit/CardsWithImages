4214d624da8a2fdd83e33c1271e444c3e05fefde
AndreiGit <84809708+AndreiGit@users.noreply.github.com>
2023-02-14 16:44:35 +0400

    add cards

--- Assets/UI/UIController.cs
+++ Assets/UI/UIController.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UI;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class UIController : UIControllerBase
 {
     private VisualElement _container;
     private DropdownField _dropdownField;
+    private Button _load;
+    private Button _cancel;
 
     void Start()
     {
         _container = GetElement<VisualElement>("Root");
         _dropdownField = GetElementFrom<DropdownField>("DropdownField", _container);
+        _load = GetElementFrom<Button>("Load", _container);
+        _cancel = GetElementFrom <Button>("Cancel", _container);
 
         _dropdownField.RegisterValueChangedCallback(evt => ChangedDropdawnValue(evt.newValue));
         _dropdownField.value = _dropdownField.choices[0];
